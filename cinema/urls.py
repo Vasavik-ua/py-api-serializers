efa7b3dd16f6e0b847a29a17f8ebb43beee854a5
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-03-11 21:06:58 +0100

    add new urls for routing of new viewset

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,19 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import MovieViewSet, GenresViewSet, ActorsViewSet, MovieSessionViewSet, CinemaHallViewSet
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("genres", GenresViewSet)
+router.register("actors", ActorsViewSet)
+router.register("movie_sessions", MovieSessionViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+
+app_name = "cinema"
