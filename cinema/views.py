ec6c47f798535180cc72beece041ffa0b714d286
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-03-11 21:07:38 +0100

    add new viewset for different request

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,45 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Movie, Actor, Genre, CinemaHall, MovieSession
+from cinema.serializers import MovieSerializer, MovieListSerializer, MovieDetailSerializer, ActorSerializer, \
+    GenreSerializer, CinemaHallSerializer, MovieSessionSerializer, MovieSessionListSerializer, \
+    MovieSessionDetailSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return MovieListSerializer
+        elif self.action == 'retrieve':
+            return MovieDetailSerializer
+        return MovieSerializer
+
+
+class ActorsViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenresViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all()
+    serializer_class = MovieSessionSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return MovieSessionListSerializer
+        elif self.action == 'retrieve':
+            return MovieSessionDetailSerializer
+        return MovieSessionSerializer
